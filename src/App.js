ddb4982e38d7fcecee800792dec4cb4d5fc76544
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-01 19:49:20 +0000

    added styling to navbar links

--- src/App.js
+++ src/App.js
@@ -38,24 +38,24 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className='bg-hero bg-fixed'>
-      <header className="flex flex-row justify-between  " >
-        <h1 className="text-3xl font-bold bg-cyan-400 w-min p-2 m-5 rounded-lg font-extrabold font-['Anton'] text-slate-900 tracking-wide">NotIMDb</h1>
+    <div className='bg-hero bg-fixed w-screen'>
+      <header className="flex flex-row border-b-[1px] border-white/30 w-screen justify-between sticky top-0 backdrop-blur" >
+        <h1 className="text-3xl bg-cyan-400 p-2 m-5 rounded-lg font-extrabold font-['Anton'] text-slate-900 tracking-wide">notIMDb</h1>
         {/* {user !== "" ? <h3>Welcome, {user.name}!</h3> : <h3></h3>} */}
-        <ul className="flex flex-row items-center">
+        <ul className="flex flex-row items-center text-white justify-self-center justify-around w-4/12 pl-40 font-['Roboto'] font-light">
           
             
-          <li className=""><Link to="/">Home</Link></li>
-          <li className=""><Link to="/about">About</Link></li>
-          <li className=""><Link to="/leaderboard">Leaderboard</Link></li>
+          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/">Home</Link></li>
+          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/about">About</Link></li>
+          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/leaderboard">Leaderboard</Link></li>
          
-          </ul>
+        </ul>
           
-          <ul className="flex flex-row items-center">
-      
-          {/* <li><Link to="/login">{user !== "" ? <p>Logout</p> : <p>Login</p>}</Link></li> */}
-          <li>{user === "" ? <Link to="/login">Login/Sign-up</Link> : <Link to="/login">Logout</Link>}</li>
-          <li>{user === "" ? <p>Not logged in</p> : <p>User: {user.name}</p>}</li>
+        <ul className="flex flex-row items-center text-white justify-self-end justify-between w-2/12 pr-10 font-['Roboto'] font-light">
+        
+            {/* <li><Link to="/login">{user !== "" ? <p>Logout</p> : <p>Login</p>}</Link></li> */}
+            <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <Link to="/login">Login/Sign-up</Link> : <Link to="/login">Logout</Link>}</li>
+            <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <p>Not logged in</p> : <p>User: {user.name}</p>}</li>
         
         </ul>
       </header>
