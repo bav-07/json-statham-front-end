5f41fdfcdc8383e7d17a8c0b3cc94af3d8ee58ad
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-02 11:43:59 +0000

    increased font weight on navbar

--- src/App.js
+++ src/App.js
@@ -15,17 +15,10 @@ import Video from './containers/Video';
 function App() {
 
   const [user, setUser] = useState("");
-
-
-
   const [users, setUsers] = useState([])
-
   const [darkMode, setDarkMode] = useState("dark");
-
-  
   const [checked, setChecked] = React.useState(true);
   
-
   useEffect(() => {
     if (darkMode === "dark") {
       document.documentElement.classList.add("dark");
@@ -39,8 +32,6 @@ function App() {
     setDarkMode(darkMode === "dark" ? "light" : "dark");
   };
 
-
-
   const fetchUserData = async () => {
             const response = await fetch ("http://localhost:8080/users");
             const data = await response.json();
@@ -49,12 +40,10 @@ function App() {
         }
 
   useEffect (() => {
-      
       fetchUserData()
   }, [user])
 
   const [movies, setMovies] = useState([])
-
 
   const fetchMovieData = async () => {
       const response = await fetch ("http://localhost:8080/movies");
@@ -75,27 +64,27 @@ function App() {
         
         <Link to="/"><h1 className="text-3xl bg-gradient-to-r from-blue-400 to-cyan-400 p-2 m-5 rounded-lg font-extrabold font-['Anton'] text-slate-900 tracking-wide">notIMDb</h1></Link>
         {/* {user !== "" ? <h3>Welcome, {user.name}!</h3> : <h3></h3>} */}
-        <ul className="flex flex-row items-center text-white justify-self-center justify-around w-4/12 pl-40 font-['Roboto'] font-light">
+        <ul className="flex flex-row items-center text-white justify-self-center justify-around w-1/2 font-['Inter'] font-light">
           
             
-          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/popular">Popular</Link></li>
-          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/about">About</Link></li>
-          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1"><Link to="/leaderboard">Leaderboard</Link></li>
+          <li className="transition-all hover:text-cyan-400 font-medium hover:underline underline-offset-4 decoration-1"><Link to="/popular">Popular</Link></li>
+          <li className="transition-all hover:text-cyan-400 font-medium hover:underline underline-offset-4 decoration-1"><Link to="/about">About</Link></li>
+          <li className="transition-all hover:text-cyan-400 font-medium hover:underline underline-offset-4 decoration-1"><Link to="/leaderboard">Leaderboard</Link></li>
          
-        </ul>
+        {/* </ul>
           
-        <ul className="flex flex-row items-center text-white justify-self-end justify-between w-2/12 pr-10 font-['Roboto'] font-light">
+        <ul className="flex flex-row items-center text-white justify-self-end justify-between w-2/12 pr-10 font-['Roboto'] font-light"> */}
         
           {/* <li><Link to="/login">{user !== "" ? <p>Logout</p> : <p>Login</p>}</Link></li> */}
-          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <Link to="/login">Login/Sign-up</Link> : <Link to="/login">Logout</Link>}</li>
-          <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <p>Not logged in</p> : <p>User: {user.name}</p>}</li>
+          <li className="transition-all hover:text-cyan-400 font-medium hover:underline underline-offset-4 decoration-1">{user === "" ? <Link to="/login">Login/Sign-up</Link> : <Link to="/login">Logout</Link>}</li>
+          <li className="transition-all hover:text-cyan-400 font-extrabold hover:cursor underline-offset-4 decoration-1">{user === "" ? <p>Not logged in</p> : <p>User: {user.name}</p>}</li>
           <Switch
-      checked={checked}
-      onChange={handleThemeSwitch}
-      inputProps={{ 'aria-label': 'controlled' }}
-    />
+            checked={checked}
+            onChange={handleThemeSwitch}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
 
-        </ul>
+      </ul>
       
       </header>
       <div className='relative'>
