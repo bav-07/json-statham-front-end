0d3d880f12fa63b7e5f2ca05f26ab4db0e68846b
Loshanth Selvanayagam <loshanth75@googlemail.com>
2023-02-02 01:21:43 +0000

    imported mui toggle switch and implemented it instead of button for dark mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import AboutContainer from './containers/AboutContainer';
 import LeaderboardContainer from './containers/LeaderboardContainer';
 import LoginContainer from './containers/LoginContainer';
 import { useEffect, useState } from 'react';
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
+
 
 function App() {
 
@@ -16,6 +19,10 @@ function App() {
 
   const [darkMode, setDarkMode] = useState("light");
 
+  
+  const [checked, setChecked] = React.useState(true);
+  
+
   useEffect(() => {
     if (darkMode === "dark") {
       document.documentElement.classList.add("dark");
@@ -24,7 +31,8 @@ function App() {
     }
   }, [darkMode]);
 
-  const handleThemeSwitch = () => {
+  const handleThemeSwitch = (event) => {
+    setChecked(event.target.checked)
     setDarkMode(darkMode === "dark" ? "light" : "dark");
   };
 
@@ -63,9 +71,12 @@ function App() {
           {/* <li><Link to="/login">{user !== "" ? <p>Logout</p> : <p>Login</p>}</Link></li> */}
           <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <Link to="/login">Login/Sign-up</Link> : <Link to="/login">Logout</Link>}</li>
           <li className="transition-all hover:text-cyan-400 hover:underline underline-offset-4 decoration-1">{user === "" ? <p>Not logged in</p> : <p>User: {user.name}</p>}</li>
-                <button className="bg-green-200 p-4 rounded-3xl" onClick={handleThemeSwitch}>
-        Dark Mode
-      </button>
+          <Switch
+      checked={checked}
+      onChange={handleThemeSwitch}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+
         </ul>
       
       </header>
