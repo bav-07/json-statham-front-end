184423e340104a915f7b17a7bcd454dc7a97528a
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-02 09:24:10 +0000

    added styling to video text

--- src/containers/Video.js
+++ src/containers/Video.js
@@ -7,7 +7,8 @@ function Video() {
         <div className="overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-10"></div>
         <video className='h-full w-full object-cover' autoPlay loop muted src={videoBg}  /> 
         <div className='content absolute w-full h-full top-0 flex flex-col items-center justify-center text-white'>
-            <h1>Guess Who Got It To Work Yeeeeeee</h1>
+            <h1 className="text-9xl font-['Inter'] font-extrabold z-50">Search. <span className='text-transparent bg-clip-text bg-gradient-to-br  from-cyan-300 to-blue-600'>Review.</span></h1>
+            <h1 className="text-5xl font-['Inter'] font-bold tracking-tight">React to movies. State your opinion.</h1>
         </div>
     </div>
   )
